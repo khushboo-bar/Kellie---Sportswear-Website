a0e1c8959ca24e44cd9827fb56161982060fcb8a
khushboo <khushboobarnolia83423@gmail.com>
2023-11-03 13:04:49 +0530

    first commit

---
+++ js/script.js
@@ -0,0 +1,17 @@
+// document.getElementById("demo").innerHTML="hello world";
+
+
+
+// var a = 2 + 5;
+// console.log(a)
+
+// document.write("hello world")
+
+
+// let class12 ={
+//     name: "khushboo",
+//     age:22,
+//     city: "ajmer",
+//     mob: 6350044350,
+// }
+
